ea31f0a98de342e25db5c8e48ae0cad41c4dff37
Djalma Xavier <djalmaxavier42@gmail.com>
2024-05-24 21:16:04 -0300

    Added user authentication check on index page and implemented login functionality

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,13 +1,14 @@
 const Login = require("../models/LoginModel");
 
 exports.index = (req, res) => {
-  res.render("login");
+  if (req.session.user) return res.render("index");
+  return res.render("login");
 };
 
 exports.register = async (req, res) => {
   try {
     const login = new Login(req.body);
-    await login.registra();
+    await login.register();
 
     if (login.erros.length > 0) {
       req.flash("erros", login.erros);
@@ -16,13 +17,40 @@ exports.register = async (req, res) => {
       });
       return;
     }
-    
+
     req.flash("sucesso", "Seu usuário foi criado com sucesso.");
     req.session.save(() => {
       return res.redirect("/login/index");
     });
-
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.erros.length > 0) {
+      req.flash("erros", login.erros);
+      req.session.save(() => {
+        return res.redirect("/login/index");
+      });
+      return;
+    }
+
+    req.flash("sucesso", "Login realizado com sucesso!");
+    req.session.user = login.users;
+    req.session.save(() => {
+      return res.redirect("/");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 };
