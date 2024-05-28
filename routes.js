1f32f38bb310c3554d2fa6d27fb334f68e83413b
Djalma Xavier <djalmaxavier42@gmail.com>
2024-05-28 16:32:11 -0300

    add contato route

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController.js");
+
+const { checkUser } = require("./src/middlewares/middleware.js");
 
 // Rotas da home
 route.get("/", homeController.index);
@@ -11,5 +14,10 @@ route.get("/login/index", loginController.index);
 route.post("/login/register", loginController.register);
 route.post("/login/enter", loginController.login);
 route.get("/login/logout", loginController.logout);
+
+//Rotas de contato
+route.get("/contato/index", checkUser, contatoController.index);
+route.post("/contato/register", checkUser, contatoController.register);
+route.get("/contato/index/:id", checkUser, contatoController.idParam);
 
 module.exports = route;
