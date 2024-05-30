f6a94bbca64b9b54d4223cd22d505bfd9e628261
Djalma Xavier <djalmaxavier42@gmail.com>
2024-05-30 18:01:23 -0300

    Add functionality to fetch and render contacts on index page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,6 @@
-exports.index = (req, res) => {
-  res.render("index");
+const Contato = require("../models/ContatoModel");
+
+exports.index = async (req, res) => {
+  const agenda = await Contato.searchContacts();
+  res.render("index", { agenda });
 };
